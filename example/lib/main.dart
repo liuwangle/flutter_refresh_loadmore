1814cbe3525789eeaae95486a6397021ef2503ef
lwl <1336000127@qq.com>
2019-07-09 15:18:54 +0800

    0.15

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
-import 'example.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_refresh_loadmore/flutter_refresh_loadmore.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,69 +11,101 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text("title"),
+        ),
+        body: Myexample(),
+      )
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Myexample extends StatefulWidget{
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  State<StatefulWidget> createState() {
+    return _MyexampleState();
   }
 
+}
+class _MyexampleState extends State<Myexample>{
+  @override
+  void initState() {
+    list.add("a");
+    list.add("a");
+    list.add("a");
+    list.add("a");
+    list.add("a");list.add("a");list.add("a");list.add("a");list.add("a");list.add("a");
+    list.add("a");list.add("a");list.add("a");list.add("a");list.add("a");
+
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Myexample(),
+    return getList();
+  }
+
+
+  GlobalKey<ListViewRefreshLoadMoreWidgetState> _listViewKey = new GlobalKey();
+  bool hasMoreData = true;
+
+  List<String> list=new List<String>();
+
+  Widget getList() {
+    return ListViewRefreshLoadMoreWidget(
+      key: _listViewKey,
+      //listview  count
+      itemCount: list.length,
+
+      //listview 的item widget
+      swrapInsideWidget: (buildContex, index) {
+        return Container(
+          margin: EdgeInsets.only(bottom: 10),
+          color: Colors.black38,
+          height: 50,
+          child: Center(child: Text("${list[index]}  ${index}"),),
+        );
+      },
+      //下拉刷新
+      refrshCallback: () async {
+        await Future.delayed(Duration(milliseconds: 1000),(){
+          list=new List();
+          list.add("b");list.add("b");list.add("a");list.add("a");list.add("a");
+          list.add("a");list.add("a");list.add("vvvv");
+          list.add("a");list.add("a");list.add("vvvv");
+          list.add("a");list.add("a");list.add("vvvv");
+          list.add("a");list.add("a");list.add("vvvv");
+        });//获取数据 添加到list
+        _listViewKey.currentState.changeData(list.length);
+      },
+      //加载更多
+      loadMoreCallback: () async {
+        await Future.delayed(Duration(milliseconds: 1000),(){
+          list.add("a");list.add("a");list.add("a");list.add("a");list.add("a");
+          list.add("a");list.add("a");list.add("a");list.add("a");list.add("a");
+          //如果加载完毕 hasMoreData设置为true
+
+        });//获取数据 添加到list
+        _listViewKey.currentState.changeData(list.length,hasMoreData: hasMoreData);
+      },
+
+      /// 加载更多 用
+      hasMoreData: hasMoreData,
+//    ///footer widget 非必须传有默认
+//    footerWidget: (statusStr){
+//      return null;
+//    },
+//    //head widget 非必传有默认
+//    headWidget: (headstate,currentHeight){
+//    return null;
+//    ;
+//    },
     );
   }
+
 }
+
